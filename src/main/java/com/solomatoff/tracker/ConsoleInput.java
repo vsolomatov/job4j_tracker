2972cbc7a5effc2e2770f99403904c689772d192
Solomatov Viacheslav <solomatoff.studio@gmail.com>
2021-10-12 11:27:07 +0500

    Initial commit

---
+++ src/main/java/com/solomatoff/tracker/ConsoleInput.java
@@ -0,0 +1,33 @@
+package com.solomatoff.tracker;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ConsoleInput implements Input {
+    private Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public String ask(String question) {
+       //System.out.print(question);
+        return scanner.nextLine();
+    }
+
+    @Override
+    public String ask(String question, ArrayList<String> range) {
+        String key = this.ask(question);
+
+        boolean exist = false;
+        for (String value : range) {
+            //System.out.println(value);
+            if (key.equals(value)) {
+                exist = true;
+                break;
+            }
+        }
+        if (exist) {
+            return key;
+        } else {
+            throw new MenuOutException("Out of menu range");
+        }
+    }
+}
